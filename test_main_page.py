49f8624d02f9e6dd84c7fb65d3737765f84fc78a
Klmchnk-K <kolomychenko.k@gmail.com>
2024-12-04 00:05:11 +0300

    Added a method for checking if an element is present

--- test_main_page.py
+++ test_main_page.py
@@ -8,3 +8,11 @@ def test_guest_can_go_to_login_page(driver):
 
     page.open()
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+
+    page = MainPage(driver, link)
+
+    page.open()
+    page.should_be_login_link()
