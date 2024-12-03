64e9bb98416ebfc77344cdb47ec7db668db5b125
Klmchnk-K <kolomychenko.k@gmail.com>
2024-12-03 22:43:23 +0300

    Changed test_main_page according to Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,10 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
 
-    driver.get(link)
+    page = MainPage(driver, link)
 
-    login_link = driver.find_element(By.XPATH, "//a[@id='login_link']")
-    login_link.click()
+    page.open()
+    page.go_to_login_page()
